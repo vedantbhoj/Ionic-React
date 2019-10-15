1e4234e05f1c9c415d2a9f58b05111e5696f62cc
Vedant Bhoj <bhoj.vedant28@gmail.com>
2019-10-15 11:24:22 -0700

    modified tabs and tab1 desc.

--- src/App.tsx
+++ src/App.tsx
@@ -49,15 +49,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={flash} />
-            <IonLabel>Tab One</IonLabel>
+            <IonLabel>IONIC REACT</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={apps} />
-            <IonLabel>Tab Two</IonLabel>
+            <IonLabel>Portfolio</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={send} />
-            <IonLabel>Tab Three</IonLabel>
+            <IonLabel>Contact me!!!</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -31,36 +31,23 @@ const Tab1: React.FC = () => {
         <IonCard className="welcome-card">
           <img src="/assets/shapes.svg" alt="" />
           <IonCardHeader>
-            <IonCardSubtitle>Get Started</IonCardSubtitle>
-            <IonCardTitle>Welcome to Ionic</IonCardTitle>
+            <IonCardSubtitle>Portfolio - new Ionic React App</IonCardSubtitle>
+            <IonCardTitle>VEDANT BHOJ | MS - Computer Engineering | San Jose State University</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
             <p>
-              Now that your app has been created, you'll want to start building out features and
-              components. Check out some of the resources below for next steps.
+            I am currently pursuing a Masters of Science in Computer Engineering at San Jose State University, California. I completed my under graduation from Vishwakarma Institute of Information Technology, Pune, India with a Bachelor's degree in Electronics and Telecommunication Engineering. My primary areas of interest include Web Development, Data Engineering and Computer Networks. I have 3 years of experience as a Programmer Analyst at Cognizant where I developed and maintained multi-layered web applications for the IT security teams. Currently interning at a Health Care startup as a Full Stack Engineer. I am proficient in JavaScript, C# and have worked with C, JAVA, MATLAB and Python before. I enjoy developing web applications and I am skilled in HTML5, CSS, JavaScript and know frameworks like Angular and ReactJS. Additionally, I have experience developing ETL data pipeline and backend.
             </p>
           </IonCardContent>
         </IonCard>
 
         <IonList lines="none">
           <IonListHeader>
-            <IonLabel>Resources</IonLabel>
+            <IonLabel>My Website</IonLabel>
           </IonListHeader>
-          <IonItem href="https://ionicframework.com/docs/" target="_blank">
+          <IonItem href="https://vedantbhoj.com/" target="_blank">
             <IonIcon slot="start" color="medium" icon={book} />
-            <IonLabel>Ionic Documentation</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
-            <IonIcon slot="start" color="medium" icon={build} />
-            <IonLabel>Scaffold Out Your App</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
-            <IonIcon slot="start" color="medium" icon={grid} />
-            <IonLabel>Change Your App Layout</IonLabel>
-          </IonItem>
-          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
-            <IonIcon slot="start" color="medium" icon={colorFill} />
-            <IonLabel>Theme Your App</IonLabel>
+            <IonLabel>Vedant's portfolio</IonLabel>
           </IonItem>
         </IonList>
       </IonContent>
