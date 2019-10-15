aff7bb115b1426bf8c6138fe7aa6a67079d6b685
Vedant Bhoj <bhoj.vedant28@gmail.com>
2019-10-15 11:32:25 -0700

    removed my profile desc.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -31,13 +31,13 @@ const Tab1: React.FC = () => {
         <IonCard className="welcome-card">
           <img src="/assets/shapes.svg" alt="" />
           <IonCardHeader>
-            <IonCardSubtitle>Portfolio - new Ionic React App</IonCardSubtitle>
+            <IonCardSubtitle>Ionic React App</IonCardSubtitle>
             <IonCardTitle>VEDANT BHOJ | MS - Computer Engineering | San Jose State University</IonCardTitle>
           </IonCardHeader>
           <IonCardContent>
-            <p>
+            {/* <p>
             I am currently pursuing a Masters of Science in Computer Engineering at San Jose State University, California. I completed my under graduation from Vishwakarma Institute of Information Technology, Pune, India with a Bachelor's degree in Electronics and Telecommunication Engineering. My primary areas of interest include Web Development, Data Engineering and Computer Networks. I have 3 years of experience as a Programmer Analyst at Cognizant where I developed and maintained multi-layered web applications for the IT security teams. Currently interning at a Health Care startup as a Full Stack Engineer. I am proficient in JavaScript, C# and have worked with C, JAVA, MATLAB and Python before. I enjoy developing web applications and I am skilled in HTML5, CSS, JavaScript and know frameworks like Angular and ReactJS. Additionally, I have experience developing ETL data pipeline and backend.
-            </p>
+            </p> */}
           </IonCardContent>
         </IonCard>
 
